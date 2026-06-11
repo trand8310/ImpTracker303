6a1511b004e252b5dd6d71289a0b87c3ba143c55
liuwei <86007829@qq.com>
2026-06-11 08:49:16 +0800

    2

---
+++ MainClient/Scheduler/DispatchLogLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainClient.Scheduler
+{
+    public enum DispatchLogLevel
+    {
+        Trace = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    }
+
+}
